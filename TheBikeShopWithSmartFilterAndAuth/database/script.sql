81bb553da794ad5c2ef74621a628c2b4078da33a
iXmalMonk <ixmalmonk@gmail.com>
2023-11-28 01:30:21 +0300

    Update

--- TheBikeShopWithSmartFilterAndAuth/database/script.sql
+++ TheBikeShopWithSmartFilterAndAuth/database/script.sql
@@ -13,5 +13,11 @@ CREATE TABLE `database`.`token`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(32) NOT NULL,
     `token` VARCHAR(32) NOT NULL,
+    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE (`email`));
+CREATE EVENT `delete_token_event`
+    ON SCHEDULE EVERY 1 MINUTE
+    DO
+    DELETE FROM `token` WHERE `date` < (NOW() - INTERVAL 1 MINUTE);
+SET GLOBAL `event_scheduler` = ON;
--- TheBikeShopWithSmartFilterAndAuth/page.php
+++ TheBikeShopWithSmartFilterAndAuth/page.php
@@ -2,7 +2,20 @@
 if (isset($_COOKIE["auth_token"]))
 {
     echo '
-    <h1>PAGE</h1>
+    <h1">PAGE</h1>
+    <input type="text" id="myInput">  
+    <button onclick="onClick()">---</button>  
+    <ul id="myUl"></ul>
+    <script>
+        function onClick() {
+            var myInput = document.getElementById("myInput").value
+            var myUl = document.getElementById("myUl")
+            myUl.innerHTML += "<li>" + myInput + "</li>"
+            //var li = document.createElement("li")
+            //li.textContent = myInput
+            //myUl.appendChild(li)
+        }
+    </script>
     ';
 }
 else
